109602f0a7b11925fadad3f24fb102cdcf5b98aa
VAdithya29 <55947904+VAdithya29@users.noreply.github.com>
2024-02-15 15:56:04 +0000

    css firx 2 BrowserRouter-> HashRouter

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import { Container } from "react-bootstrap";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
