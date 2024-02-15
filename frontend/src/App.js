14345c6013ffe5930b0fd7ea6c3178ffc48ddb7f
VAdithya29 <55947904+VAdithya29@users.noreply.github.com>
2024-02-15 16:09:29 +0000

    Reverted all changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouterRouter as Router, Route, Routes } from "react-router-dom";
 import { Container } from "react-bootstrap";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
